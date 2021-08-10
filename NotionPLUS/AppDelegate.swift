8b4024c21beca44e5913fa3439e64eacdad372b0
Kyle Zhong <kylenzhong98@gmail.com>
2021-08-10 14:20:13 -0700

    Initial Commit

---
+++ NotionPLUS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NotionPLUS
+//
+//  Created by Kyle Zhong on 8/10/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NotionPLUS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NotionPLUS
+//
+//  Created by Kyle Zhong on 8/10/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
